8041679a2f34603b1ecf814897bd36768e8ff5c6
unknown <ahmedaslam.alphasquad@gmail.com>
2023-10-27 22:07:01 +0500

    new update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import gptlogo from './assets/chatgpt.svg'
+import addbtn from './assets/add-30.png'
+import msgIcon from './assets/message.svg'
+import home from './assets/home.svg'
+import saved from './assets/bookmark.svg'
+import upgrade from './assets/rocket.svg'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="sidebar">
+        <div className="upperside ">
+          <div className="uppersidetop">
+            <img src={gptlogo} alt="logo" className="logo" />
+            <span className="brand ">ChatGPT</span>
+          </div>
+          <button className="midbtn">
+            <img src={addbtn} alt="new chat" className="addbtn" />
+            New Chat
+          </button>
+          <div className="uppersidebottom">
+            <button className="query">
+              <img src={msgIcon} alt="" />
+              What is programming?
+            </button>
+            <button className="query">
+              <img src={msgIcon} alt="" />
+              How to use a API?
+            </button>
+          </div>
+        </div>
+        <div className="lowerside">
+            <div className="listitmes"><img src={home} alt="" className="listitemimg" />Home</div>
+            <div className="listitmes"><img src={saved} alt="" className="listitemimg" />Saved</div>
+            <div className="listitmes"><img src={upgrade} alt="" className="listitemimg" />Upgrade to pro</div>
+
+
+        </div>
+      </div>
+      <div className="main"></div>
     </div>
   );
 }
